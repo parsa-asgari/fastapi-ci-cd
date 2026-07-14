abe980fb41588ace7b9b303b3bfc24c0bcf18298
Parsa Asgari <parsa.asgari3264@gmail.com>
2026-07-14 19:04:19 +0330

    feat(api): better metrics response

--- app/test_main.py
+++ app/test_main.py
@@ -35,6 +35,9 @@ async def test_metrics():
         "os": "5.15.167.4-microsoft-standard-WSL2",
         "runtime": "Docker",
         "framework": "FastAPI",
+        "language": "Python",
+        "version": "0.1.0",
+        "release": "2024-06-01",
     }
 
 
